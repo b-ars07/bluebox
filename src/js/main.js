140097fec68b1c4e4a75aeb87b31010a56a539a4
Arseniy Berezin <b-ars07@yandex.ru>
2017-04-13 02:21:29 +0300

    сверстал шапку

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,17 @@
-@@include('lib/lib.js')
+document.ready(function () {
+    @@include('lib/lib.js')
+    
+    
+    $('nav-item>a').on('click', function (evt) {
+        evt.preventDefault();
+        
+        $('nav-item>a').removeClass('active-link');
+        $(this).addClass('active-link');
+    });
+    
+    
+});
+
+
+
 
