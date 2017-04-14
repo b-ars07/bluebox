2422e198593b2c9fc229b98af5aae244825c169e
Arseniy Berezin <b-ars07@yandex.ru>
2017-04-15 02:49:13 +0300

    Сверстал 3 блока, подключил swiper.js

--- src/js/lib/lib.js
+++ src/js/lib/lib.js
@@ -1,3 +1,4 @@
 @@include('../../../frontend/bower_components/jquery/dist/jquery.min.js')
 @@include('../../../frontend/bower_components/tether/dist/js/tether.min.js')
 @@include('../../../frontend/bower_components/bootstrap/dist/js/bootstrap.min.js')
+@@include('../../../frontend/bower_components/swiper/dist/js/swiper.min.js')
--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,7 @@
-document.ready(function () {
-    @@include('lib/lib.js')
+@@include('lib/lib.js')
+$(document).ready(function () {
     
-    
+    //делаем ссылку меню активной при нажатии
     $('nav-item>a').on('click', function (evt) {
         evt.preventDefault();
         
@@ -9,7 +9,21 @@ document.ready(function () {
         $(this).addClass('active-link');
     });
     
+    $('.submenu-link').on('click', function (evt) {
+        evt.preventDefault();
+        
+        $('.submenu-link').removeClass('submenu-link_active');
+        $(this).addClass('submenu-link_active');
+    });
     
+    var mySwiper = new Swiper ('.main-slider', {
+      // Navigation arrows
+      nextButton: '.swiper-button-next',
+      prevButton: '.swiper-button-prev',
+      slidesPerView: auto,
+      loop: true,
+    
+  })        
 });
 
 
