5959b1dc2b3ed146737c338cce08e14e02f93dd2
Arseniy Berezin <b-ars07@yandex.ru>
2017-04-16 03:52:15 +0300

    добавил fancybox, сверстал блок клиентов

--- src/js/lib/lib.js
+++ src/js/lib/lib.js
@@ -1,4 +1,5 @@
 @@include('../../../frontend/bower_components/jquery/dist/jquery.min.js')
 @@include('../../../frontend/bower_components/tether/dist/js/tether.min.js')
 @@include('../../../frontend/bower_components/bootstrap/dist/js/bootstrap.min.js')
-@@include('../../../frontend/bower_components/swiper/dist/js/swiper.min.js')
+@@include('../../../frontend/bower_components/swiper/dist/js/swiper.jquery.min.js')
+@@include('../../../frontend/bower_components/fancybox/dist/jquery.fancybox.min.js')
--- src/js/main.js
+++ src/js/main.js
@@ -1,31 +1,36 @@
 @@include('lib/lib.js')
 $(document).ready(function () {
     
+
     //делаем ссылку меню активной при нажатии
     $('nav-item>a').on('click', function (evt) {
         evt.preventDefault();
-        
+
         $('nav-item>a').removeClass('active-link');
         $(this).addClass('active-link');
     });
-    
+
     $('.submenu-link').on('click', function (evt) {
         evt.preventDefault();
-        
+
         $('.submenu-link').removeClass('submenu-link_active');
         $(this).addClass('submenu-link_active');
     });
+
+    var swiper1 = new Swiper('.main-slider', {
+        // Navigation arrows
+        nextButton: '.main-slider .swiper-button-next',
+        prevButton: '.main-slider .swiper-button-prev',
+        loop: true,
+
+    });
+
+    $('.fancybox').fancybox();
     
-    var mySwiper = new Swiper ('.main-slider', {
-      // Navigation arrows
-      nextButton: '.swiper-button-next',
-      prevButton: '.swiper-button-prev',
-      slidesPerView: auto,
-      loop: true,
+     var swiper2 = new Swiper('.clients-slider', {
+        nextButton: '.clients-slider .swiper-button-next',
+        prevButton: '.clients-slider .swiper-button-prev',
+        slidesPerView: 4,
+    });
     
-  })        
 });
-
-
-
-
